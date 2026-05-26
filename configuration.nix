43a8ecf63045e9c3a9af23cffb7bf368950849e5
calendar <1131821081@qq.com>
2026-05-26 21:24:24 +0800

    fix: revert to stable 25.05 option names for GNOME

--- configuration.nix
+++ configuration.nix
@@ -38,8 +38,8 @@
 
   # ---------- 桌面环境 (GNOME) ----------
   services.xserver.enable = true;
-  services.displayManager.gdm.enable = true;
-  services.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
   services.xserver.xkb = {
     layout = "cn";
