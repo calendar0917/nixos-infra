174944ec6615f8959bca7c433bd81d9db8b582d1
1131821081@qq.com <1131821081@qq.com>
2026-06-16 17:31:10 +0800

    update mirror

--- flake.nix
+++ flake.nix
@@ -1,6 +1,5 @@
 {
   description = "NixOS configuration";
-
   inputs = {
     nixpkgs.url = "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/nixos-unstable/nixexprs.tar.xz";
     home-manager = {
@@ -18,7 +17,22 @@
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
       modules = [
-        # Overlay: 注入自定义包（必须在 HM 之前）
+              {
+          nix.settings = {
+            substituters = [
+              "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+              "https://cache.nixos.org"
+            ];
+            trusted-substituters = [
+              "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+            ];
+            trusted-public-keys = [
+              "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+              "tuna.tsinghua.edu.cn-1:Dk5GQyD1H6hLm1W7q1Z6Tp8J9nK7yR4vF2X3E5B7N1M="
+            ];
+          };
+        }
+         # Overlay: # 注入自定义包（必须在 HM 之前）
         ({ pkgs, ... }: {
           nixpkgs.overlays = [
             (final: prev: {
